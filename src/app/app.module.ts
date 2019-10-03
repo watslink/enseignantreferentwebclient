2b19cb0fa67d33600d4f37dca9826a7deda385d5
watslink <seb.decroix@gmail.com>
2019-10-03 13:58:59 +0200

    generate composant login & inscription

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule
