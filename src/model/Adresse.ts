b99b4def302d9c4f8233db068beb20ff2e74af1b
watslink <seb.decroix@gmail.com>
2019-10-08 13:50:19 +0200

    add Adresse model

---
+++ src/model/Adresse.ts
@@ -0,0 +1,12 @@
+export class Adresse {
+
+  public AdresseId: number;
+  public voie: string;
+  public ville: string;
+  public numero: number;
+  public codePostal: number;
+
+  constructor() {}
+
+
+}
