e7364b5b097674d16b3625f11fe553330eb611f7
watslink <seb.decroix@gmail.com>
2019-10-08 13:58:32 +0200

    add RepresentantLegal model

---
+++ src/model/RepresentantLegal.ts
@@ -0,0 +1,14 @@
+import {Adresse} from './Adresse';
+
+export class RepresentantLegal {
+
+  public representantLegalId: number;
+  public identite: string;
+  public telephone: string;
+  public mail: string;
+  public adresse: Adresse;
+
+  constructor() {}
+
+
+}
