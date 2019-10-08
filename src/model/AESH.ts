13e89e360fd36fb5c6dfecf69449e0020b3fc86b
watslink <seb.decroix@gmail.com>
2019-10-08 14:01:41 +0200

    add AESH model

---
+++ src/model/AESH.ts
@@ -0,0 +1,15 @@
+import {PIAL} from './PIAL';
+
+export class AESH {
+
+  public aeshId: number;
+  public nom: string;
+  public prenom: string;
+  public mail: string;
+  public telephone: string;
+  public pial: PIAL;
+
+  constructor() {}
+
+
+}
