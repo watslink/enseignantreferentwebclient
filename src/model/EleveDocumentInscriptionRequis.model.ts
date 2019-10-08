3e7f04a8f9b9427ca672f92d2ba81d95abd8f4e4
watslink <seb.decroix@gmail.com>
2019-10-08 14:20:25 +0200

    add EleveDocumentInscriptionRequis model

---
+++ src/model/EleveDocumentInscriptionRequis.model.ts
@@ -0,0 +1,12 @@
+import {DocumentInscriptionRequis} from './DocumentInscriptionRequis.model';
+
+export class EleveDocumentInscriptionRequis {
+
+ public documentInscriptionRequis: DocumentInscriptionRequis;
+ public lien: string;
+ public ok: boolean;
+
+  constructor() {}
+
+
+}
