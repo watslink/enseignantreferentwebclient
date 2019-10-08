b833a198617d44d6bb7e5c18a566da972097656c
watslink <seb.decroix@gmail.com>
2019-10-08 14:13:55 +0200

    add Document model

---
+++ src/model/Document.model.ts
@@ -0,0 +1,13 @@
+import {Categorie} from './Categorie.model';
+
+export class Document {
+
+  public documentId: number;
+  public nom: string;
+  public lien: string;
+  public categorie: Categorie;
+
+  constructor() {}
+
+
+}
