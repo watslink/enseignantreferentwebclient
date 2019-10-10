5ac96ed3433cc4c50d3c375298abdb2c58a47932
watslink <seb.decroix@gmail.com>
2019-10-10 11:30:27 +0200

    add MaterielPedagoAdapteService

---
+++ src/service/materielpedagoadapte.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {MaterielPedagoAdapte} from '../model/MaterielPedagoAdapte.model';
+
+
+@Injectable()
+export class MaterielPedagoAdapteService {
+
+  private host = 'http://localhost:8080';
+
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getListMaterielPedagoAdapte(): Observable<MaterielPedagoAdapte[]> {
+    return this.http.get<MaterielPedagoAdapte[]>(this.host + '/materielPedagoAdaptes');
+  }
+  getMaterielPedagoAdapte(id: number): Observable<MaterielPedagoAdapte> {
+    return this.http.get<MaterielPedagoAdapte>(this.host + '/materielPedagoAdapte/' + id);
+  }
+  addMaterielPedagoAdapte(materielPedagoAdapte: MaterielPedagoAdapte) {
+    return this.http.post(this.host + '/materielPedagoAdapte/', materielPedagoAdapte, {observe: 'response'});
+  }
+  updateStrutcure(materielPedagoAdapte: MaterielPedagoAdapte) {
+    return this.http.put(this.host + '/materielPedagoAdapte/', materielPedagoAdapte, {observe: 'response'});
+  }
+  deleteMaterielPedagoAdapte(id: number) {
+    return this.http.delete(this.host + '/materielPedagoAdapte/' + id, {observe: 'response'} );
+  }
+}
