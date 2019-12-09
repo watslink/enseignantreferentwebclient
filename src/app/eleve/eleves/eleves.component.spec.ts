a79dcd7fc7404c1f1d1c091ca93540ba4e9fcf04
watslink <seb.decroix@gmail.com>
2019-12-09 18:04:06 +0100

    add eleves component

---
+++ src/app/eleve/eleves/eleves.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ElevesComponent } from './eleves.component';
+
+describe('ElevesComponent', () => {
+  let component: ElevesComponent;
+  let fixture: ComponentFixture<ElevesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ElevesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ElevesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/eleve/eleves/eleves.component.ts
@@ -0,0 +1,145 @@
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  PipeTransform,
+  ViewChild
+} from '@angular/core';
+
+
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {MDBModalRef, MDBModalService, MdbTableDirective, MdbTablePaginationComponent} from 'angular-bootstrap-md';
+import {faPencilAlt, faTimes} from '@fortawesome/free-solid-svg-icons';
+import {faAddressCard} from '@fortawesome/free-solid-svg-icons/faAddressCard';
+import {Eleve} from '../../../model/Eleve.model';
+import {EleveService} from '../../../service/eleve.service';
+import {AuthenticationService} from '../../../service/authentication.service';
+
+
+
+
+@Component({
+  selector: 'app-eleves',
+  templateUrl: './eleves.component.html',
+  styleUrls: ['./eleves.component.css']
+})
+export class ElevesComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective;
+  @ViewChild(MdbTablePaginationComponent, { static: true }) mdbTablePagination: MdbTablePaginationComponent;
+
+  fapencil = faPencilAlt;
+  facard = faAddressCard;
+  fadelete = faTimes;
+  public eleves: Eleve[] = [];
+  public elevesData: Eleve[] = [];
+  searchText = '';
+  previous: string;
+
+  maxVisibleItems = 8;
+  modalRef: MDBModalRef;
+
+  navigationSubscription;
+
+  constructor(private authServ: AuthenticationService,
+              private eleveServ: EleveService,
+              private router: Router,
+              private cdRef: ChangeDetectorRef,
+              private route: ActivatedRoute,
+              private modalService: MDBModalService) {
+    this.navigationSubscription = this.router.events.subscribe((e: any) => {
+      if (e instanceof NavigationEnd) {
+        this.ngOnInit();
+      }
+    });
+  }
+
+  @HostListener('input') oninput() {
+    this.mdbTablePagination.searchText = this.searchText;
+  }
+
+  ngOnInit() {
+    const list = this.route.snapshot.params.select;
+    if ( list === 'tous') {
+      this.refreshAll();
+    }
+    if ( list === 'nonvus') {
+      this.refreshNonVus();
+    }
+    if ( list === 'vus') {
+      this.refreshVus();
+    }
+  }
+  ngAfterViewInit() {
+    this.mdbTablePagination.setMaxVisibleItemsNumberTo(this.maxVisibleItems);
+    this.mdbTablePagination.calculateFirstItemIndex();
+    this.mdbTablePagination.calculateLastItemIndex();
+    this.cdRef.detectChanges();
+  }
+  searchItems() {
+    const prev = this.mdbTable.getDataSource();
+
+    if (!this.searchText) {
+      this.mdbTable.setDataSource(prev);
+      this.eleves = this.mdbTable.getDataSource();
+    }
+
+    if (this.searchText) {
+      this.eleves = this.mdbTable.searchLocalDataBy(this.searchText);
+      this.mdbTable.setDataSource(this.mdbTable.getDataSource());
+    }
+  }
+  openEleveDetails(element: Eleve) {
+  }
+  openEleveEdit(element: Eleve) {
+  }
+  openModalDelete(element: Eleve) {
+  }
+  refreshAll() {
+    this.eleveServ.getListEleveInscrits(parseInt(localStorage.getItem('idEnsRef'), 10)).subscribe(
+      element => {
+        this.elevesData = element;
+        this.mdbTable.setDataSource(this.elevesData);
+        this.eleves = this.mdbTable.getDataSource();
+        this.previous = this.mdbTable.getDataSource();
+      },
+      err => {
+        this.authServ.logout();
+        this.router.navigateByUrl('/login');
+      });
+  }
+  refreshNonVus() {
+    this.eleveServ.getListEleveNonVus(parseInt(localStorage.getItem('idEnsRef'), 10)).subscribe(
+      element => {
+        this.elevesData = element;
+        this.mdbTable.setDataSource(this.elevesData);
+        this.eleves = this.mdbTable.getDataSource();
+        this.previous = this.mdbTable.getDataSource();
+      },
+      err => {
+        this.authServ.logout();
+        this.router.navigateByUrl('/login');
+      });
+  }
+  refreshVus() {
+    this.eleveServ.getListEleveVus(parseInt(localStorage.getItem('idEnsRef'), 10)).subscribe(
+      element => {
+        this.elevesData = element;
+        this.mdbTable.setDataSource(this.elevesData);
+        this.eleves = this.mdbTable.getDataSource();
+        this.previous = this.mdbTable.getDataSource();
+      },
+      err => {
+        this.authServ.logout();
+        this.router.navigateByUrl('/login');
+      });
+  }
+  ngOnDestroy() {
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
+    }
+  }
+}
