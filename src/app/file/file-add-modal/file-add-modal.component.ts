3299c441af506d0a0261ea80e0dc9a75c392b63f
watslink <seb.decroix@gmail.com>
2019-12-11 13:26:00 +0100

    update file add modal

--- src/app/file/file-add-modal/file-add-modal.component.ts
+++ src/app/file/file-add-modal/file-add-modal.component.ts
@@ -4,6 +4,7 @@ import {FileService} from '../../../service/file.service';
 import {Eleve} from '../../../model/Eleve.model';
 import {EleveDocumentInscriptionRequis} from '../../../model/EleveDocumentInscriptionRequis.model';
 import {EleveService} from '../../../service/eleve.service';
+import {Document} from '../../../model/Document.model';
 
 
 @Component({
@@ -15,27 +16,53 @@ export class FileAddModalComponent implements OnInit {
   fileToUpload: File;
   eleve: Eleve;
   eleveDocRequis: EleveDocumentInscriptionRequis;
+  doc: Document;
+  newDoc: Document;
+  type;
   constructor(public modalRef: MDBModalRef,
               private fileService: FileService,
               private eleveService: EleveService
               ) { }
 
   ngOnInit() {
+    if (this.type === 'addDoc') {
+      this.newDoc = new Document();
+    }
   }
 
   save() {
     const eleveDirectory = this.eleve.nom + '-' + this.eleve.prenom;
-    this.fileService.addFile(this.fileToUpload,
-      eleveDirectory, this.eleveDocRequis.documentInscriptionRequis.nom).subscribe( res => {
-      for (const eleveDocReq of this.eleve.listEleveDocumentsInscriptionRequis) {
-        if (eleveDocReq.documentInscriptionRequis === this.eleveDocRequis.documentInscriptionRequis) {
-          eleveDocReq.extension = this.fileToUpload.name.substr(this.fileToUpload.name.lastIndexOf ('.') + 1);
-          eleveDocReq.ok = true;
-          this.eleveService.updateEleve(this.eleve).subscribe();
+    if (this.type === 'eleveDocRequis') {
+      this.fileService.addFile(this.fileToUpload,
+        eleveDirectory, this.eleveDocRequis.documentInscriptionRequis.nom).subscribe(res => {
+        for (const eleveDocReq of this.eleve.listEleveDocumentsInscriptionRequis) {
+          if (eleveDocReq.documentInscriptionRequis === this.eleveDocRequis.documentInscriptionRequis) {
+            eleveDocReq.extension = this.fileToUpload.name.substr(this.fileToUpload.name.lastIndexOf('.') + 1);
+            eleveDocReq.ok = true;
+            this.eleveService.updateEleve(this.eleve).subscribe();
+          }
         }
-
-      }
-    });
+      });
+    }
+    if (this.type === 'updateDoc') {
+      this.fileService.addFile(this.fileToUpload,
+        eleveDirectory, this.doc.nom).subscribe(res => {
+        for (const doc of this.eleve.listDocuments) {
+          if (doc === this.doc) {
+            doc.extension = this.fileToUpload.name.substr(this.fileToUpload.name.lastIndexOf('.') + 1);
+            this.eleveService.updateEleve(this.eleve).subscribe();
+          }
+        }
+      });
+    }
+    if (this.type === 'addDoc') {
+      this.fileService.addFile(this.fileToUpload,
+        eleveDirectory, this.newDoc.nom).subscribe(res => {
+        this.newDoc.extension = this.fileToUpload.name.substr(this.fileToUpload.name.lastIndexOf('.') + 1);
+        this.eleve.listDocuments.push(this.newDoc);
+        this.eleveService.updateEleve(this.eleve).subscribe();
+      });
+    }
     this.modalRef.hide();
   }
 
